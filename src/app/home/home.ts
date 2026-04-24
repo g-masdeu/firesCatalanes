dc3861ba033c2b3e9ba69798eab957e23be28063
g-masdeu <guillem.masdeu97@gmail.com>
2026-04-24 17:22:21 +0200

    detall fira 2

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -11,16 +11,32 @@ import { RegionFairsListComponent } from '../region-fairs-list/region-fairs-list
   styleUrl: './home.css'
 })
 export class HomeComponent implements OnInit {
-  firesSetmana: any[] = [];
+  firesProperes: any[] = [];
 
   ngOnInit() {
-    const hoy = new Date();
-    const mesDiaHoy = hoy.toISOString().substring(5, 10); // Ejemplo: "04-24"
+    this.filtrarFiresProperes();
+  }
 
-    this.firesSetmana = FAIRS.filter((f: any) => {
-      const mesFira = f.iniDate.substring(5, 7); // Extrae el mes del JSON
-      const mesActual = (hoy.getMonth() + 1).toString().padStart(2, '0');
-      return mesFira === mesActual;
+  filtrarFiresProperes() {
+    const llistaDatesValides: string[] = [];
+    
+    for (let i = 0; i <= 10; i++) {
+      const d = new Date();
+      d.setDate(d.getDate() + i);
+      
+      const mm = (d.getMonth() + 1).toString().padStart(2, '0');
+      const dd = d.getDate().toString().padStart(2, '0');
+      
+      llistaDatesValides.push(`${mm}-${dd}`);
+    }
+
+    this.firesProperes = FAIRS.filter((f: any) => {
+      const fMMDD = f.iniDate.substring(5, 10); 
+      return llistaDatesValides.includes(fMMDD);
+    });
+
+    this.firesProperes.sort((a, b) => {
+      return a.iniDate.substring(5, 10).localeCompare(b.iniDate.substring(5, 10));
     });
   }
 }
